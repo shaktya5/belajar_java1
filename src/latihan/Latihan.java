573e7b3b09860e98ee387216aa5337b8e4b5e8a2
shaktya5 <daffaalghifari810@gmail.com>
2024-02-01 09:44:12 +0700

    Praktikum Pbo - Java - First Commit

---
+++ src/latihan/Latihan.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan {
+    public static void main(String[] args) {
+    System.out.println("Hello Warga MU!, Cageur, Bageur, Pinter");
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+    }
+    
+}
